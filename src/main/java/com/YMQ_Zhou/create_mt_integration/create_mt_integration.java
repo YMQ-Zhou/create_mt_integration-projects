66851e291c9db40682128f571ef4ef247c30e9a9
雨漫倾周 <756857921@qq.com>
2025-07-10 09:03:37 +0800

    删除多余代码，修改前置错误

--- src/main/java/com/YMQ_Zhou/create_mt_integration/create_mt_integration.java
+++ src/main/java/com/YMQ_Zhou/create_mt_integration/create_mt_integration.java
@@ -30,12 +30,15 @@ import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(create_mt_integration.MODID)
+
 public class create_mt_integration
+
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "create_mt_integration";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
+/*
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
@@ -55,8 +58,8 @@ public class create_mt_integration
     // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
     public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
             .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
+           .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+     .displayItems((parameters, output) -> {
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
@@ -124,4 +127,6 @@ public class create_mt_integration
             LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
         }
     }
+
+ */
 }
